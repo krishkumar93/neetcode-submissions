20058b7cabaa0ebc1c825fc45aaa83e8f5c0fa9b
Krish Kumar <139633086+krishkumar93@users.noreply.github.com>
2026-09-22 10:51:44 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    bool isAnagram(string s, string t) {
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
+        // if (s == t) return true;
+        // return false;
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int>m1;
+        unordered_map<char,int>m2;
+
+        for(char c:s){
+            m1[c]++;
+        }
+        for(char c:t){
+            m2[c]++;
+        }
+
+        for(int i=0;i<s.size();i++){
+            if(m1[s[i]]!=m2[s[i]]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
